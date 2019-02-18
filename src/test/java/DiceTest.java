d88b92d81dde58e5b57eddd25470697c1c26ead9
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-02-18 14:28:42 -0500

    added empty test files and set up project settings

---
+++ src/test/java/DiceTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DiceTest {
+
+    @Test
+    public void test1() {
+        Assert.assertEquals(10, 10);
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertEquals(15, 15);
+    }
+}
